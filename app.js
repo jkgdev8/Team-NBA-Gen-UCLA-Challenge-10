f72742b91cf0fb78d9aa5ae84b25174c67a62c45
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-02 23:35:35 -0800

    function passes

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function startHtml() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Team Profile</title>
+        <title>Create Your NBA TEAM!</title>
     </head>
     <body>
         <nav class="navbar navbar-dark bg-dark mb-5">
@@ -181,3 +181,4 @@ function finishHtml() {
 }
 
 initApp();
+//
